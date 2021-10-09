0d6533fd6fc47420839e0dd679497ec5c59f031c
26071999 <89468211+26071999@users.noreply.github.com>
2021-10-09 11:25:45 +0530

    Update Candies.java
    Commit changes into Eclipse

--- src/Candies.java
+++ src/Candies.java
@@ -16,6 +16,8 @@ public class Candies {
             else {
                 System.out.println("INVALID INPUT");
                 System.out.println("NUMBER OF CANDIES LEFT :"+N);
+                 System.out.println("Hi Hello");
+                
         }
     }
 }
